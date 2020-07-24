36d246905aac9db58ab416dbd170d36769e19c08
Alex <superalex0802@gmail.com>
2020-07-25 02:07:37 +0300

    Update GameCenterBot.py

--- GameCenterBot.py
+++ GameCenterBot.py
@@ -22,13 +22,17 @@ async def hello(ctx):
 
 
 
-
 @bot.command()
 async def привет(ctx):
 	author = ctx.message.author
 	await ctx.send('Привет, ' + author.mention)
 
 
+@bot.command()
+@commmands.has_permissions(administrator = True)
+async def mute_role(ctx, member: discord.Member);
+    mute_role = discord.utils.get(ctx.message.guild.roles, name = '𝕄𝕦𝕥𝕖🔇')
+    await member.add_roles(mute_role)
 
 
 

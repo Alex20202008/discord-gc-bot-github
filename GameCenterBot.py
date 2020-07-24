f2ac6a87d307f1831d3e4e36d2ab6e16c4627720
Alex <66314039+Alex20202008@users.noreply.github.com>
2020-07-24 23:57:28 +0300

    add bot

---
+++ GameCenterBot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import time
+import os
+
+token = os.environ.get('HIDE_BOT_TOKEN') # no u
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print('Bot is online. Be happy :)')
+
+@bot.command()
+async def send(ctx, *, arg):
+    await ctx.send(arg)
+
+
+
+bot.run(token)

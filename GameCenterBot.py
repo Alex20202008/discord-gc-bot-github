a8b115609890cdd9aef51d7b5d9bd1d64ce1d398
Alex <superalex0802@gmail.com>
2020-07-28 19:37:16 +0300

    Update GameCenterBot.py

--- GameCenterBot.py
+++ GameCenterBot.py
@@ -15,12 +15,11 @@ async def on_ready():
     activity = discord.Activity(name='Не трогайте бота', type=1)
     await bot.change_presence(activity=activity)# .Status.(Idle, do_not_disturb, online)
 
-@bot.command
+@bot.command()
 async def avatar(ctx, member: discord.Member):
     author = ctx.message.author
     embed = discord.Embed( description='**Аватарка пользователя ' + str(member.mention) + '**', colour=discord.Colour.purple())
     embed.set_image(url=member.avatar_url)
-
 
 @bot.command()
 async def send(ctx, *, arg):

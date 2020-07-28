056e6a644ba9a69c0d0498ea1172e14d25b895de
Alex <superalex0802@gmail.com>
2020-07-28 19:51:55 +0300

    Update GameCenterBot.py

--- GameCenterBot.py
+++ GameCenterBot.py
@@ -16,12 +16,10 @@ async def on_ready():
 
 @bot.command()
 async def avatar(ctx, member: discord.Member):
-	author = ctx.message.author
-	embed = discord.Embed( description='**Аватарка пользователя ' + str(member.mention) + '**', colour=discord.Colour.purple())
-	embed.set_image(url=member.avatar_url)
-
-
-	await ctx.send(embed=embed)
+    author = ctx.message.author
+    embed = discord.Embed( description='**Аватарка пользователя ' + str(member.mention) + '**', colour=discord.Colour.purple())
+    embed.set_image(url=member.avatar_url)
+    await ctx.send(embed=embed)
 
 
 @bot.command()

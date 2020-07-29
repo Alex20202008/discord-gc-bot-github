e91d05f918cf6aa86730e26dbf911ae10107a648
Alex <superalex0802@gmail.com>
2020-07-29 20:31:21 +0300

    Update GameCenterBot.py

--- GameCenterBot.py
+++ GameCenterBot.py
@@ -48,8 +48,9 @@ async def kiss_other(ctx, member: discord.Member, member1: discord.Member):
     author = ctx.message.author
     if author != member :
         # ctx.send(str(author.mention) + ' и ' str(member.mention) + ' поцеловались')
-        embed = discord.Embed( description='**' + str(member.mention) + ' и ' + str(member1.mention) + ' поцеловались, а кто у нас тут шипперит, ' + str(author.mention) + '?**', colour=discord.Colour.red())
+        embed = discord.Embed( description='**' + str(member.mention) + ' и ' + str(member1.mention) + ' поцеловались**', colour=discord.Colour.red())
         embed.set_image(url='https://i.pinimg.com/originals/69/cf/45/69cf45b7947fe8318ee8c899873066cd.gif')
+        embed.set_footer(text='**А кто у нас тут шипперит, ' + str(author.mention) + '?**'')
         await ctx.send(embed=embed)
     else:
         if member == member1:

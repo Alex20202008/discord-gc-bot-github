da7fd896a04af8adcc35286621e6443e6a693446
Alex <superalex0802@gmail.com>
2020-08-01 02:59:04 +0300

    Update GameCenterBot.py

--- GameCenterBot.py
+++ GameCenterBot.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='!')
 @bot.event
 async def on_ready():
     print('Bot is online. Be happy :)')
-    activity = discord.Activity(name=' -> 💻 <-', type=1)
+    activity = discord.Activity(name=' -->  💻  <--', type=1)
     await bot.change_presence(activity=activity)# .Status.(Idle, do_not_disturb, online) | , status=discord.Status.do_not_disturb
 
 @bot.command()

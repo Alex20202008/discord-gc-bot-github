0e727a5016ca2f0f5fefbefa6d6720709a715ebb
Alex <superalex0802@gmail.com>
2020-08-03 00:34:31 +0300

    Update GameCenterBot.py

--- GameCenterBot.py
+++ GameCenterBot.py
@@ -187,7 +187,7 @@ async def embed(ctx):
     embed.add_field(name="hello", value="This has the bot say hello", inline=False)
     embed.add_field(name="Test Field 2", value="this is test 2", inline=False)
     embed.add_field(name="test field 3", value="This is a test 3", inline=False)
-    embed.set_footer(text="Made by MagicalAlex", icon_url='https://cdn.discordapp.com/avatars/717480562155192381/df15e97304ee47e04fe912b22a5ebdd5.jpg?size=1024')
+    embed.set_footer(text="Made by MagicalAlex", icon_url='https://cdn.discordapp.com/avatars/717480562155192381/501b447f922b915e51b6d037fa97badb.jpg?size=1024')
     await ctx.send(embed=embed)
 
 @bot.command(pass_context=True)
